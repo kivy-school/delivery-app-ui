2bf38a8fbeb0c1b155e20a77a48a04d5026b2a44
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-11-05 18:10:20 -0300

    ✨ feat: Loading app theme and updating the Window size on delivery_app/__init__.py

--- delivery_app/__init__.py
+++ delivery_app/__init__.py
@@ -1,8 +1,14 @@
+from kivy.core.window import Window
 from kivy_reloader.app import App
 
-from delivery_app.screens.main_screen import MainScreen
+from delivery_app.screens.splash_screen import SplashScreen
+from delivery_app.theme import load_theme
+
+load_theme()
+
+Window.size = (414, 896)
 
 
 class DeliveryApp(App):
     def build(self):
-        return MainScreen()
+        return SplashScreen()
