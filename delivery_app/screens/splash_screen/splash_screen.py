909ddd5c3fbf5d72ccec32d9f312274360487bef
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-11-05 18:05:57 -0300

    ✨ feat: Created Splash Screen

--- delivery_app/screens/splash_screen/splash_screen.py
+++ delivery_app/screens/splash_screen/splash_screen.py
@@ -3,13 +3,14 @@ import os
 from kivy.uix.screenmanager import Screen
 from kivy_reloader.utils import load_kv_path
 
-main_screen_kv = os.path.join(
+splash_screen_kv = os.path.join(
     'delivery_app',
     'screens',
-    'main_screen.kv',
+    'splash_screen',
+    'splash_screen.kv',
 )
-load_kv_path(main_screen_kv)
+load_kv_path(splash_screen_kv)
 
 
-class MainScreen(Screen):
+class SplashScreen(Screen):
     pass
